baf2a3d4abe6e38f6f679eb901a24be5abc50444
Juan Cruz Venier Rojas <juancruzvenier@gmail.com>
2023-05-13 19:11:32 -0300

    entrega final js

---
+++ js/app.js
@@ -0,0 +1,81 @@
+const shopContent = document.getElementById("shopContent");
+
+const verCarrito = document.getElementById("verCarrito");
+
+const modalContainer = document.getElementById("modal-container");
+
+let carrito = [];
+
+productos.forEach((product)=> {
+    let content = document.createElement("div");
+    content.className = "card"
+    content.innerHTML = `
+    <img src="${product.img}" width=200px >
+    <h3>${product.nombre}</h3>
+    <p class="price">$${product.precio}</p>
+    `;
+
+    shopContent.append(content);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "Agregar al Carrito";
+    comprar.className = "agregarCarrito"
+
+    content.append(comprar);
+
+
+
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+        console.log(carrito); 
+    });
+
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Carrito</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "X";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", ()=> {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+carrito.forEach((product) => {
+    
+    let carritoContent = document.createElement("div");
+    carritoContent.className = "modal-content";
+    carritoContent.innerHTML = `
+    <img src="${product.img}" width=200px>
+    <h3>${product.nombre}</h3>
+    <p>$${product.precio}</p>
+    `;
+
+    modalContainer.append(carritoContent);
+});
+
+const total = carrito.reduce((acc, el) => acc + el.precio, 0) ;
+
+const totalBuying = document.createElement("div");
+totalBuying.className = "total-content";
+totalBuying.innerHTML = `total a pagar: $${total}
+<a href="./pages/checkout.html"><button class="finalizarCompra">Finalizar Compra</button></a>`;
+modalContainer.append(totalBuying);
+
+});
---
+++ js/productos.js
@@ -0,0 +1,45 @@
+
+
+const productos = [
+    {
+        id: 1,
+        nombre:"Adidas X Speedportal FG",
+        precio: 35000,
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/b784de2eaac742db94bfae9b010e7e1b_9366/Botines_X_SPEEDPORTAL.1_Terreno_Firme_Verde_GW8426_01_standard_hover.jpg",
+    },
+
+    {
+        id: 2,
+        nombre:"Gilbert Sidestep",
+        precio: 37400,
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/250/458/products/sidestep-x15-6-stud1-4ded376ec4393a5d8116718216311307-640-0.webp",
+    },
+
+    {
+        id: 3,
+        nombre:"Nike Superfly 8",
+        precio: 55000,
+        img: "https://www.opensports.com.ar/media/catalog/product/cache/4769e4d9f3516e60f2b4303f8e5014a8/D/J/DJ3977-506_0.jpg",
+    },
+
+    {
+        id: 4,
+        nombre:"Puma Future Z",
+        precio: 32500,
+        img: "https://cdn.media.amplience.net/i/frasersdev/20702570_o.jpg?v=220909212010",
+    },
+
+    {
+        id: 5,
+        nombre:"Adidas Messi",
+        precio: 39200,
+        img: "https://newsport.vtexassets.com/arquivos/ids/15469383/GW8390-a.jpg?v=638109449181300000",
+    },
+
+    {
+        id: 6,
+        nombre:"Adidas X Speedportal Futsal",
+        precio: 34500,
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/3e359e7cc2874a8bb53dae9e010656b4_9366/Botines_X_Speedportal.3_Cancha_Cubierta_Verde_GW8464.jpg",
+    },
+];
